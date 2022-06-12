58bb31fe94e37f54367e028015a178a0f007ed4f
shakedx <kolesnikov_danil123@mail.ru>
2022-06-12 18:06:43 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(DateTime.IsLeapYear(year));
+        }
+    }
+}
